a12f7da198dabc3562803d5ae82db092729aa30d
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-07-10 16:50:50 -0400

    Adds connect button

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{cell::RefCell, rc::Rc};
-
 use wasm_p2p::{
-    console_log, wasm_bindgen, wasm_bindgen_futures, Closure, ConnectionUpdate, JsCast, JsValue,
-    P2P,
+    wasm_bindgen,
+    wasm_bindgen_futures::{self, spawn_local},
+    Closure, ConnectionUpdate, JsCast, JsValue, P2P,
 };
 use web_sys::{window, HtmlInputElement};
 
@@ -14,57 +14,24 @@ extern "C" {
 }
 
 fn main() {
+    console_error_panic_hook::set_once();
     wasm_bindgen_futures::spawn_local(main_async());
 }
 
 async fn main_async() {
-    console_error_panic_hook::set_once();
     let mut p2p = P2P::new("wss://signaling.luisherasme.com");
     let id = p2p.id().await;
-    console_log!("Your peer id: {}", id);
-
-    let answer = confirm("Do youa wnt to send a connection request?")
-        .as_bool()
-        .unwrap();
-
-    if answer {
-        let peer_id = prompt("What is the ID of the peer you want to connect to?")
-            .as_string()
-            .unwrap();
-
-        p2p.connect(&peer_id).await;
-    }
-
-    let document = window().unwrap().document().unwrap();
-
     let other_peer_id: Rc<RefCell<Option<String>>> = Rc::new(RefCell::new(None));
 
-    let callback_p2p = p2p.clone();
-    let callback_other_peer_id = other_peer_id.clone();
-
-    let callback = Closure::<dyn FnMut()>::new(move || {
-        let document = window().unwrap().document().unwrap();
-        let message_input = document
-            .get_element_by_id("message-input")
-            .unwrap()
-            .dyn_into::<HtmlInputElement>()
-            .unwrap();
-
-        let message = message_input.value();
-        message_input.set_value("");
-
-        if let Some(peer_id) = callback_other_peer_id.borrow().as_ref() {
-            callback_p2p.send(&peer_id, &message);
-        }
-
-        insert_message(message, "You", "blue");
-    });
-
-    let chat_form = document.get_element_by_id("chat-form").unwrap();
-    chat_form
-        .add_event_listener_with_callback("submit", callback.as_ref().unchecked_ref())
-        .unwrap();
-    callback.forget();
+    window()
+        .unwrap()
+        .document()
+        .unwrap()
+        .get_element_by_id("peer-id")
+        .unwrap()
+        .set_inner_html(&id);
+    setup_connect_button(p2p.clone());
+    setup_chat_input(p2p.clone(), other_peer_id.clone());
 
     loop {
         let (messages, connections) = p2p.update().await;
@@ -72,17 +39,16 @@ async fn main_async() {
         for connection in connections {
             match connection {
                 ConnectionUpdate::Connected(peer_id) => {
-                    console_log!("Peer {} connected.", peer_id);
+                    insert_message(format!("Peer {} connected.", peer_id), "SYSTEM", "black");
                     other_peer_id.borrow_mut().replace(peer_id);
                 }
                 ConnectionUpdate::Disconnected(peer_id) => {
-                    console_log!("Peer {} disconnected.", peer_id);
+                    insert_message(format!("Peer {} disconnected.", peer_id), "SYSTEM", "black");
                 }
             }
         }
 
-        for (peer_id, message) in messages {
-            console_log!("Peer {} says {}", peer_id, message);
+        for (_peer_id, message) in messages {
             insert_message(message, "Peer", "red");
         }
     }
@@ -104,4 +70,61 @@ fn insert_message(message: String, name: &str, color: &str) {
 
     messages_div.append_child(&element).unwrap();
     element.scroll_into_view();
+}
+
+fn setup_connect_button(p2p: P2P) {
+    let document = window().unwrap().document().unwrap();
+
+    let callback = Closure::<dyn FnMut()>::new(move || {
+        let mut p2p = p2p.clone();
+        spawn_local(async move {
+            if let Some(peer_id) =
+                prompt("What is the ID of the peer you want to connect to?").as_string()
+            {
+                p2p.connect(&peer_id).await;
+            }
+        });
+    });
+
+    document
+        .get_element_by_id("connect")
+        .unwrap()
+        .add_event_listener_with_callback("click", callback.as_ref().unchecked_ref())
+        .unwrap();
+
+    callback.forget();
+}
+
+fn setup_chat_input(p2p: P2P, other_peer_id: Rc<RefCell<Option<String>>>) {
+    let callback_p2p = p2p.clone();
+    let callback_other_peer_id = other_peer_id.clone();
+
+    let callback = Closure::<dyn FnMut()>::new(move || {
+        let document = window().unwrap().document().unwrap();
+        let message_input = document
+            .get_element_by_id("message-input")
+            .unwrap()
+            .dyn_into::<HtmlInputElement>()
+            .unwrap();
+
+        let message = message_input.value();
+        message_input.set_value("");
+
+        if let Some(peer_id) = callback_other_peer_id.borrow().as_ref() {
+            callback_p2p.send(&peer_id, &message);
+        }
+
+        insert_message(message, "You", "blue");
+    });
+
+    window()
+        .unwrap()
+        .document()
+        .unwrap()
+        .get_element_by_id("chat-form")
+        .unwrap()
+        .add_event_listener_with_callback("submit", callback.as_ref().unchecked_ref())
+        .unwrap();
+
+    callback.forget();
 }
